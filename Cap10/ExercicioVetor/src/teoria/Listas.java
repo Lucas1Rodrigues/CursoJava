de4e4794a2b481b244fd9d1e6e43a35fbb375999
Lucas <lucasrodriguezy@gmail.com>
2023-05-26 17:03:27 -0300

    Iniciando sobre listas

---
+++ Cap10/ExercicioVetor/src/teoria/Listas.java
@@ -0,0 +1,33 @@
+package teoria;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Listas {
+
+	public static void main(String[] args) {
+		// demonstracao de listas e a classe array list
+		
+		List<String> list = new ArrayList<>();
+		
+		list.add("Lucas");
+		list.add("Fulano");
+		list.add("cicrano");
+		list.add("aninha");
+		list.add(0,"Danyelly");
+		
+		
+		System.out.println(list.size());
+		for(String x : list) {
+			System.out.println(x);
+		}
+		
+		System.out.println("----------------------");
+		list.removeIf(x -> x.charAt(0) == 'L');
+		for (String string : list) {
+			System.out.println(string);
+		}
+
+	}
+
+}
