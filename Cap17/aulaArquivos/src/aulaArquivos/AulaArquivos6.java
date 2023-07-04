2a225f6a396528b219d85184eb00bb1b4cdfaadd
Lucas <lucasrodriguezy@gmail.com>
2023-07-04 17:27:46 -0300

    trying print tabuada.txt

--- Cap17/aulaArquivos/src/aulaArquivos/AulaArquivos6.java
+++ Cap17/aulaArquivos/src/aulaArquivos/AulaArquivos6.java
@@ -1,10 +1,28 @@
 package aulaArquivos;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class AulaArquivos6 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Ler o arquivo tabuada.txt	
+		File path = new File("/home/lucas/eclipse/CursoJavaNelio/Cap17/tabuada.txt");
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(path))){
+			String line = br.readLine();
+			
+			while(line != null) {
+				System.out.println(line);
+				br.readLine();
+			}
+		}
+		catch(IOException e) {
+			System.out.println(" ERROR " + e.getMessage());
+		}
+		
 	}
 
 }
