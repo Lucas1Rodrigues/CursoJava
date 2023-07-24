670c6338abf9479c8321e7433a5e83c3347380c4
Lucas <lucasrodriguezy@gmail.com>
2023-07-24 15:02:48 -0300

    Learning functions into function

--- Cap20/lambdaExpressions/src/lambdaExpressions/Program.java
+++ Cap20/lambdaExpressions/src/lambdaExpressions/Program.java
@@ -6,6 +6,7 @@ import java.util.function.Consumer;
 
 import entities.Product;
 import util.IncreasePrice;
+import util.SumOfPredicate;
 
 public class Program {
 
@@ -15,11 +16,13 @@ public class Program {
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
-		Double factor = 1.1;
-	
-		list.forEach(p -> p.setPrice(p.getPrice()*factor));
 		
-		list.forEach(System.out::println);
+		SumOfPredicate sp = new SumOfPredicate();
+	
+		double sum = sp.filteredSum(list, p -> p.getPrice() >= 250);
+		
+		System.out.println("Sum = " + String.format("%.2f", sum));
+		
 		}
 
 	}
