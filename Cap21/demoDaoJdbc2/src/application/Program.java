ec3124395ffdd6b205dfaad95cc16f5751462091
Lucas <lucasrodriguezy@gmail.com>
2023-08-08 15:09:09 -0300

    findAll implementing

--- Cap21/demoDaoJdbc2/src/application/Program.java
+++ Cap21/demoDaoJdbc2/src/application/Program.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.dao.impl.SellerDaoImplJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -19,6 +20,12 @@ public class Program {
 		System.out.println("==== TEST 2: seller findbyDepartment ====\n");
 		Department department = new Department(2,null);
 		List<Seller> list = sellerdao.findByDepartment(department);
+		for (Seller obj: list) {
+			System.out.println(obj);
+		}
+		System.out.println();
+		System.out.println("==== TEST 3: seller findAll ====\n");
+		list = sellerdao.findAll();
 		for (Seller obj: list) {
 			System.out.println(obj);
 		}
